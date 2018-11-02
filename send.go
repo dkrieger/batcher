0888607eb841d22c49d7caa1b18e6dfd10b4c2f6
Doug Krieger <doug@cashortrade.org>
2018-11-02 16:16:15 -0400

    "redistreams" -> "redistream"

--- send.go
+++ send.go
@@ -4,7 +4,7 @@ func (b *Batcher) SendBatch(name string) {
 	// read MaxSize items from batch stream, move to ready stream (which an
 	// external consumer watches)
 	batch, _ := b.batches.Load(name).(Batch)
-	streamClient := redistreams.NewWrapper(b.redisClient)
+	streamClient := redistream.WrapClient(b.redisClient)
 	streams, err := streamClient.Consume("batcher", "batcher", batch.config.MaxSize, []string{name, ">"})
 	if err != nil {
 		panic(err)
@@ -13,5 +13,5 @@ func (b *Batcher) SendBatch(name string) {
 	aggregated := AggregateBatch(stream)
 	streamClient.Produce(aggregated, b.batchDest)
 }
-func AggregateBatch(entries []*redistreams.Entry) *redistreams.Entry {
+func AggregateBatch(entries []*redistream.Entry) *redistream.Entry {
 }
