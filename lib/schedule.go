c458f10538bff1a3a20a1eb3e6efd49cb0b7b404
Doug Krieger <doug@cashortrade.org>
2018-11-12 14:56:56 -0500

    debug lockPool unlock

--- lib/schedule.go
+++ lib/schedule.go
@@ -17,6 +17,45 @@ func (b *Batcher) ScheduleBatch(name string, signals <-chan batchSignal) {
 	}
 	paused := false
 	lastSend := time.Time{}
+	defaultFunc := func() {
+		// throttle loop
+		time.Sleep(time.Second)
+		if paused {
+			return
+		}
+		// limit concurrency
+		b.lockPool.Lock()
+		defer func() {
+			time.Sleep(b.BatcherDelay())
+			b.lockPool.Unlock()
+		}()
+
+		batches := b.getBatches()
+		batch := batches[name]
+		conf := batch.config
+		metrics := batch.metrics
+		if metrics.LastSend.After(lastSend) {
+			lastSend = metrics.LastSend
+		}
+		// if !conf.Active {
+		// 	break Outer
+		// }
+		// sleep until next time batch should be sent
+		interval := conf.TargetInterval
+		now := time.Now()
+		time.Sleep(lastSend.Add(interval).Sub(now))
+		err := b.SendBatch(name)
+		if err == nil {
+			lastSend = time.Now()
+			lastSendStr := strconv.Itoa(int(lastSend.Unix()))
+			_, err := b.redisClient.HSet(b.MetricsPrefix()+"lastSend", name, lastSendStr).Result()
+			if err != nil {
+				stderr.Printf("ScheduleBatch() error:\ndetails: HSET lastSend\n%#v\n", err)
+			}
+		} else {
+			stderr.Printf("SendBatch() error: \n%#v\ntime: %#v\n", err, time.Now())
+		}
+	}
 Outer:
 	for {
 		select {
@@ -33,43 +72,7 @@ Outer:
 				paused = false
 			}
 		default:
-			// throttle loop
-			time.Sleep(time.Second)
-			if paused {
-				continue
-			}
-			// limit concurrency
-			b.lockPool.Lock()
-			defer func() {
-				time.Sleep(b.BatcherDelay())
-				b.lockPool.Unlock()
-			}()
-
-			batches := b.getBatches()
-			batch := batches[name]
-			conf := batch.config
-			metrics := batch.metrics
-			if metrics.LastSend.After(lastSend) {
-				lastSend = metrics.LastSend
-			}
-			// if !conf.Active {
-			// 	break Outer
-			// }
-			// sleep until next time batch should be sent
-			interval := conf.TargetInterval
-			now := time.Now()
-			time.Sleep(lastSend.Add(interval).Sub(now))
-			err := b.SendBatch(name)
-			if err == nil {
-				lastSend = time.Now()
-				lastSendStr := strconv.Itoa(int(lastSend.Unix()))
-				_, err := b.redisClient.HSet(b.MetricsPrefix()+"lastSend", name, lastSendStr).Result()
-				if err != nil {
-					stderr.Printf("ScheduleBatch() error:\ndetails: HSET lastSend\n%#v\n", err)
-				}
-			} else {
-				stderr.Printf("SendBatch() error: \n%#v\ntime: %#v\n", err, time.Now())
-			}
+			defaultFunc()
 		}
 		runtime.Gosched()
 	}
